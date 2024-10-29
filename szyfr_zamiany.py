ac35651657168118ac790aa35d7234d5d58579a4
KukielkaKamil <kukielka.kamil77@gmail.com>
2024-10-29 12:40:39 +0100

    Dodano Auto Hill Method

--- szyfr_zamiany.py
+++ szyfr_zamiany.py
@@ -34,14 +34,11 @@ def decrypt(text, key, alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
 dn_text = decrypt(en_text,key)
 print(dn_text)
 
+def get_rand_key(shift, alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+    key = ''.join(list(alf[:shift]) + random.sample(alf[shift:],len(alf[shift:])))
+    return key
 
-key1 = ''.join(list(alf[:17]) + random.sample(alf[17:],len(alf[17:])))
 
-# for i in range(5):
-#     key1 = ''.join(list(alf[:17]) + random.sample(alf[17:],len(alf[17:])))
-#     print(key1)
-
-en_text_1 = encrypt('THISISATESTSTRING',key1)
 
 Scorer = ngram.ngram_score('english_bigrams.txt', sep= ' ')
 
@@ -50,8 +47,8 @@ def attack_auto(crypto_text,stalych_liter = 17):
     best_score = -99999
     result = ''
     tt0 = tm()
-    while tm() - tt0 <5:
-        rand_key = ''.join(list(alf[:stalych_liter]) + random.sample(alf[stalych_liter:],len(alf[stalych_liter:])))
+    while tm() - tt0 <20:
+        rand_key = get_rand_key(stalych_liter)
         decrypted_text = decrypt(crypto_text,rand_key)
         sc = Scorer.score(decrypted_text)
         if sc > best_score:
@@ -63,15 +60,38 @@ def attack_auto2(crypto_text,stalych_liter = 17, attempts = 1000):
     best_score = -99999
     result = ''
     for i in range(attempts):
-        rand_key = ''.join(list(alf[:stalych_liter]) + random.sample(alf[stalych_liter:],len(alf[stalych_liter:])))
+        rand_key = get_rand_key(stalych_liter)
         decrypted_text = decrypt(crypto_text,rand_key)
         sc = Scorer.score(decrypted_text)
         if sc > best_score:
             best_score, result = sc, decrypted_text
     return best_score, result
 
+def change_of_key(old_key, stalych_liter=17):
+    i,j = sorted(random.sample(list(range(stalych_liter,26)),2))
+    key_new = old_key[:i] + old_key[j] + old_key[i+1:j] + old_key[i] + old_key[j+1:]
+    return key_new
+
+def attackHillClimbing(crypto_text, stalych_liter = 17):
+    best_score = -99999
+    old_key = get_rand_key(stalych_liter)
+    result = ''
+    tt0 = tm()
+    while tm() - tt0 <5:
+        rand_key = change_of_key(old_key,stalych_liter)
+        decrypted_text = decrypt(crypto_text,rand_key)
+        sc = Scorer.score(decrypted_text)
+        if sc > best_score:
+            best_score, result, old_key = sc, decrypted_text, rand_key
+    return best_score, result
+
+key1 = get_rand_key(20,alf)
+
+
+en_text_1 = encrypt('THISISATESTSTRINGTHATISREALLYAWESOME',key1)
+
 t0 = tm()
-print(attack_auto(en_text_1,17))
+print(attackHillClimbing(en_text_1,20))
 t1 = tm()
 
 print(f'czas {t1-t0} sekund')
